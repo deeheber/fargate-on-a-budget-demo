1845be76d9288ab7e1ccbb90d81b19ac56160a89
Danielle Heberling <deeheber@users.noreply.github.com>
2025-02-14 11:55:41 -0800

    Base setup for public ALB > Fargate

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Fargate Scheduler Demo
 
-Did you ever need to have a web app server on AWS for an application, but also it won't be used during certain times of the day and you want to save money running that server?
+Do you need to have a non-business critical web app that requires a server on AWS, but also it won't be used during certain times of the day? Do you want to save some money?
 
-This is one of many a solutions to this usecase that utilizes the following AWS services:
+This is a solution for this usecase that utilizes the following AWS services:
 
 - Application Load Balancer
-- Fargate
+- Fargate (Spot instances)
 - EventBridge Scheduler
 - AWS Cloud Development Kit (CDK) for deployment
+
+In this solution we heavily prefer Fargate Spot instances and have a Scheduler that shuts down the Fargate task during times of non usage.
+
+## How to run
+
+Coming soon ™️
+
+## Architecture diagram
+
+Coming soon ™️
--- lib/fargate-scheduler-demo-stack.ts
+++ lib/fargate-scheduler-demo-stack.ts
@@ -1,12 +1,98 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns'
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import {
+  Cluster,
+  ContainerImage,
+  FargateTaskDefinition,
+  LogDrivers,
+} from 'aws-cdk-lib/aws-ecs'
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { Vpc } from 'aws-cdk-lib/aws-ec2'
 
 export class FargateSchedulerDemoStack extends Stack {
+  readonly id: string
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
+    this.id = id
 
+    this.buildApplicationLoadBalancedFargateService()
     // TODO:
-    // Build applicaton load balanced Fargate service
     // Build EventBridge scheduler to shut the service off/on
+  }
+
+  private buildApplicationLoadBalancedFargateService() {
+    const vpc = new Vpc(this, `${this.id}-vpc`, {
+      vpcName: `${this.id}-vpc`,
+      maxAzs: 2,
+    })
+
+    const cluster = new Cluster(this, `${this.id}-cluster`, {
+      clusterName: `${this.id}-cluster`,
+      vpc,
+    })
+
+    const taskDefinition = new FargateTaskDefinition(
+      this,
+      `${this.id}-task-def`,
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+      }
+    )
+    taskDefinition.addContainer(`${this.id}-container`, {
+      containerName: `${this.id}-container`,
+      image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      logging: LogDrivers.awsLogs({
+        streamPrefix: `${this.id}`,
+        logGroup: new LogGroup(this, `/ecs/${this.id}-container`, {
+          logGroupName: `${this.id}-log-group`,
+          retention: RetentionDays.ONE_WEEK,
+          removalPolicy: RemovalPolicy.DESTROY,
+        }),
+      }),
+      portMappings: [
+        {
+          containerPort: 80,
+        },
+      ],
+      // Allows for a Fargate spot graceful shutdown
+      stopTimeout: Duration.seconds(120),
+    })
+
+    const service = new ApplicationLoadBalancedFargateService(
+      this,
+      `${this.id}-service`,
+      {
+        assignPublicIp: true,
+        capacityProviderStrategies: [
+          {
+            capacityProvider: 'FARGATE_SPOT',
+            weight: 50,
+          },
+          // Backup in case FARGATE_SPOT is not available
+          {
+            capacityProvider: 'FARGATE',
+            weight: 1,
+          },
+        ],
+        cluster,
+        circuitBreaker: {
+          enable: true,
+          rollback: true,
+        },
+        desiredCount: 1,
+        minHealthyPercent: 100,
+        taskDefinition,
+      }
+    )
+
+    // Allow shorter de-registration to seperate the LB from Fargate instance in the case of a spot shutdown
+    // Mostly just needs to be less than 2 minutes
+    service.targetGroup.setAttribute(
+      'deregistration_delay.timeout_seconds',
+      '30'
+    )
   }
 }
